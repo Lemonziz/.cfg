7c81a05fdf0be80957bd4a3c126ab7f86cc07f07
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2024-09-15 13:56:44 +0800

    make backup to bak, move all the files to cfgfiles

---
+++ install.sh
@@ -0,0 +1,32 @@
+#;w!/bin/zsh
+
+# Define a function which rename a `target` file to `target.backup` if the file
+# exists and if it's a 'real' file, ie not a symlink
+backup() {
+  target=$1
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.bak"
+      echo "-----> Moved your old $target config file to $target.bak"
+    fi
+  fi
+}
+
+symlink() {
+  file=$1
+  link=$2
+  if [ ! -e "$link" ]; then
+    echo "-----> Symlinking your new $link"
+    ln -s $file $link
+  fi
+}
+
+# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
+# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+for name in vimrc gitconfig tmux.conf zshrc; do
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    backup $target
+    symlink $PWD/cfgfiles/$name $target
+  fi
+done
