80d039586b7ced4e023af3bbba77288c5c7560a3
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2024-11-26 20:18:44 +0800

    installsh

--- install.sh
+++ install.sh
@@ -22,34 +22,33 @@ fi
 # Define a function which rename a `target` file to `target.backup` if the file
 # exists and if it's a 'real' file, ie not a symlink
 backup() {
-  target=$1
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      mv "$target" "$target.bak"
-      echo "-----> Moved your old $target config file to $target.bak"
+    target=$1
+    if [ -e "$target" ]; then
+        if [ ! -L "$target" ]; then
+            mv "$target" "$target.bak"
+            echo "-----> Moved your old $target config file to $target.bak"
+        fi
     fi
-  fi
 }
 
 symlink() {
-  file=$1
-  link=$2
-  if [ ! -e "$link" ]; then
-    echo "-----> Symlinking your new $link"
-    ln -s $file $link
-  fi
+    file=$1
+    link=$2
+    if [ ! -e "$link" ]; then
+        echo "-----> Symlinking your new $link"
+        ln -s $file $link
+    fi
 }
 # git submodule update --recursive this is the command for update
 # For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
 # and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
 for name in zsh_custom_commands.sh vim vimrc gitconfig tmux.conf zshrc fzf tmux; do
-  if [ ! -d "$name" ]; then
-    target="$HOME/.$name"
-    backup $target
-    symlink $PWD/cfgfiles/$name $target
-  fi
+    if [ ! -d "$name" ]; then
+        target="$HOME/.$name"
+        backup $target
+        symlink $PWD/cfgfiles/$name $target
+    fi
 done
-
 
 # install nvm
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
