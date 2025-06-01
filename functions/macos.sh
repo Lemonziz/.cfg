440b7db7f6b57783d3380cdbbb4d59e296a0df72
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2025-06-01 14:46:27 +0800

    feat: asm-lsp added

--- functions/macos.sh
+++ functions/macos.sh
@@ -1,6 +1,6 @@
 install_dependency_macos() {
     brew update
-    brew install ninja gettext cmake unzip ripgrep curl zsh luarocks tmux
+    brew install ninja gettext cmake unzip ripgrep curl zsh luarocks tmux rust
 }
 
 install_neovim_macos_arm64() {
